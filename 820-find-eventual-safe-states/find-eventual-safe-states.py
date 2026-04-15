5bde48224e434f03b4a04deafdfab21d6ca3ea1f
vishnupriya8996-maker <vishnupriya8996@gmail.com>
2026-04-15 11:12:24 +0530

    Time: 51 ms (48.12%) | Memory: 25.5 MB (82.72%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.py
+++ 820-find-eventual-safe-states/find-eventual-safe-states.py
@@ -1,7 +1,7 @@
 class Solution:
     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
-        #topological sort
-        n=len(graph)
+        #dfs
+        """n=len(graph)
         v=[0]*n
         pv=[0]*n
         check=[0]*n
@@ -24,4 +24,32 @@ class Solution:
         for i in range(n):
             if check[i]==1:
                 safe.append(i)
-        return safe
+        return safe"""
+        #bfs
+        n=len(graph)
+        out=[0]*n
+        revg=[[]for i in range(n)]
+        for u in range(n):
+            out[u]=len(graph[u])
+            for v in graph[u]:
+                revg[v].append(u)
+        q=[]
+        for i in range(n):
+            if out[i]==0:
+                q.append(i)
+        safe=[False]*n
+        while q:
+            node=q.pop(0)
+            safe[node]=True
+            for prev in revg[node]:
+                out[prev]-=1
+                if out[prev]==0:
+                    q.append(prev)
+        res=[]
+        for i in range(n):
+            if safe[i]:
+                res.append(i)
+        return res
+
+
+
