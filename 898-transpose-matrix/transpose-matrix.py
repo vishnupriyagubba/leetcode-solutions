42963f20c96c0a00a8eb5005440690ecf112b226
vishnupriyagubba <vishnupriya8996@gmail.com>
2026-05-03 15:54:35 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (78.27%) - LeetSync

---
+++ 898-transpose-matrix/transpose-matrix.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def transpose(self, matrix):
+        rows = len(matrix)
+        cols = len(matrix[0])
+        ans = [[0]*rows for _ in range(cols)]
+        for i in range(rows):
+            for j in range(cols):
+                ans[j][i] = matrix[i][j]
+        return ans
+        
